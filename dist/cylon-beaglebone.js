488b65169c25e3d2ce5c13080816b6ef7021fb29
Edgar O Silva <oswaldo.silva@gmail.com>
2013-12-03 14:19:47 -0800

    Added dependencies for gpio.

--- dist/cylon-beaglebone.js
+++ dist/cylon-beaglebone.js
@@ -9,8 +9,33 @@
 
 (function() {
   'use strict';
-  exports.awesome = function() {
-    return 'awesome';
+  var GPIO,
+    __slice = [].slice;
+
+  require("./beaglebone");
+
+  GPIO = require("cylon-gpio");
+
+  module.exports = {
+    adaptor: function() {
+      var args;
+      args = 1 <= arguments.length ? __slice.call(arguments, 0) : [];
+      return (function(func, args, ctor) {
+        ctor.prototype = func.prototype;
+        var child = new ctor, result = func.apply(child, args);
+        return Object(result) === result ? result : child;
+      })(Cylon.Adaptor.Beaglebone, args, function(){});
+    },
+    driver: function() {
+      var args;
+      args = 1 <= arguments.length ? __slice.call(arguments, 0) : [];
+      return GPIO.driver.apply(GPIO, args);
+    },
+    register: function(robot) {
+      Logger.debug("Registering Beaglebone adaptor for " + robot.name);
+      robot.registerAdaptor('cylon-beaglebone', 'beaglebone');
+      return GPIO.register(robot);
+    }
   };
 
 }).call(this);
